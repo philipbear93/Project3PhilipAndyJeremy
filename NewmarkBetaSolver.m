fb50eb53ed395a9b9f30995749bfaadfb92ed41f
aturner05 <turner.152@wright.edu>
2016-12-09 18:43:28 -0500

    updated Newmarkbetasolver and plots

--- NewmarkBetaSolver.m
+++ NewmarkBetaSolver.m
@@ -1,28 +1,66 @@
-function [ POS,VEL,ACC,T ] = NewmarkBetaSolver( K,C,M,R,Zeta,dt,t,teff,gamma,beta )
+function [ POS,VEL,ACC,T ] = NewmarkBetaSolver( K,M,Zeta,dt,t,teff,gamma,beta )
 % NewmarkBetaSolver calculates D, Dp, and Dpp for t with dt spacing using
 % the Newmark Beta method with parameters gamma and beta under the load R
 % for duration teff.
 % POS, VEL and ACC are the position, velocity and acceleration at time T
+close all
+clc
 
+stripdof = [3 4 5];
+nodes = 50;
+
+for i = 1:nodes
+    strip1 = stripdof(1)+6*i;
+    strip2 = strip1+1;
+    strip3 = strip2+1;
+    strip = [strip1 strip2 strip3];
+    stripdof = [stripdof strip];    
+end
+
+bcs = [1 2 152];
+K = full(K);
+K(stripdof,:) = [];
+K(:,stripdof) = [];
+K(bcs,:) = [];
+K(:,bcs) = [];
+M = full(M);
+M(stripdof,:) = [];
+M(:,stripdof) = [];
+M(bcs,:) = [];
+M(:,bcs) = [];
+
+%% Determine Damping matrix with Chosen Zeta value, K & M
+    Zeta = 0; %Damping Ratio
+
+    %Determine Mode Shapes and Eigenvalues
+    [mode, lam ]= eig(K);
+    w = sqrt(lam);
+
+    lam_mat = 2*Zeta*w;
+
+    C = (mode')^-1*lam_mat*mode^-1;
+    
+R = zeros(size(K,1),1);
+R(149,1) = 100000;
 
 % Assume initial deflection and velocity are zero
 Dn=zeros(size(K,1),1);
 Dpn=zeros(size(K,1),1);
 
 % Calculate Dppn0 using F=ma;
-Dppn=full(M\R);
+Dppn=M\R;
 
 % Create force matrix for after teff
 R0=zeros(size(R,1),size(R,2));
 
 T=0:dt:t;
-I=size(T,2);
+I=length(T);
 
 % Loop over time
 for i=1:I
-    POS(i,:,:)=Dn(:,:);
-    VEL(i,:,:)=Dpn(:,:);
-    ACC(i,:,:)=Dppn(:,:);
+    POS(:,i)=Dn(:);
+    VEL(:,i)=Dpn(:);
+    ACC(:,i)=Dppn(:);
     tc=T(i);
     % Calculate Dn1
     if tc <= teff
@@ -37,8 +75,36 @@ for i=1:I
     Dpn=real(Dpn1);
     Dppn=real(Dppn1);
 end
-POS(I,:,:)=Dn(:,:);
-VEL(I,:,:)=Dpn(:,:);
-ACC(I,:,:)=Dppn(:,:);
+POSITION=POS(121,:)
+VELOCITY=VEL(121,:)
+ACCELERATION=ACC(121,:)
 
+%% Plot 
+figure(1)
+fig_name1 = ['Position Plot Using Average Acceleration Method for 50 elements'];
+plot(T,POSITION)
+grid on
+ylabel('\Theta_{z41}  (rad)')
+xlabel('t(s)')
+title(fig_name1)
+saveas(gcf,fig_name1,'jpg')
+
+
+figure(2)
+fig_name2 = ['Velocity Plot Using Average Acceleration Method for 50 Elements'];
+plot(T, VELOCITY)
+grid on
+ylabel('d\Theta_{z41}/dt  (rad/s)')
+xlabel('t(s)')
+title(fig_name2)
+saveas(gcf,fig_name2,'jpg')
+
+figure(3)
+fig_name3 = ['Acceleration Plot Using Average Acceleration Method for 50 elements'];
+plot(T, ACCELERATION)
+grid on
+ylabel('d^{2}\Theta_{z41}/dt^{2}  (rad/s^{2})')
+xlabel('t(s)')
+title(fig_name3)
+saveas(gcf,fig_name3,'jpg')
 end
--- Newmark_Method.m
+++ Newmark_Method.m
@@ -3,34 +3,50 @@ close all
 clear all
 clc
 
-global Kr
-global Mr
-global Fr
-global K
-global M
-global R
-global nodes
 
 run wfem
 
 %% Count number of elements
 numofelem = size(nodes,1)-1;
-dof = (size(nodes,1)*6)-9;
+dof = 237;%(size(nodes,1)*6)-9;
 %% Pull Force Vector & Stiffness and Mass Matricies from wfem 
-K = full(Kr);
-M = full(Mr);
-R = full(Fr);
+K = full(K);
+M = full(M);
+
+stripdof = [3 4 5];
+nodes = 50;
+
+for i = 1:nodes
+    strip1 = stripdof(1)+6*i;
+    strip2 = strip1+1;
+    strip3 = strip2+1;
+    strip = [strip1 strip2 strip3];
+    stripdof = [stripdof strip];    
+end
+
+bcs = [1 2 152];
+K(stripdof,:) = [];
+K(:,stripdof) = [];
+K(bcs,:) = [];
+K(:,bcs) = [];
+M(stripdof,:) = [];
+M(:,stripdof) = [];
+M(bcs,:) = [];
+M(:,bcs) = [];
+
+R = zeros(size(K,1),1);
+R(149,1) = 100000;
 
 %% Select an appropriate dt, gamma and beta
-t = 0.02;
-dt = .00002; %Delta t in seconds
+t = 0.1;
+dt = .0001; %Delta t in seconds
 teff = .01;
 
 Gamma = [1/2 1/2 1/2];
 Beta = [1/4 1/6 1/12];
 
 %% Determine Damping matrix with Chosen Zeta value, K & M
-    Zeta = .02; %Damping Ratio
+    Zeta = 0; %Damping Ratio
 
     %Determine Mode Shapes and Eigenvalues
     [mode, lam ]= eig(K);
@@ -42,36 +58,46 @@ Beta = [1/4 1/6 1/12];
     
 
 %% Newmark Method
-
-i = 1;
-for i=1:3
-[POS,VEL,ACC,T] = NewmarkBetaSolver(K,C,M,R,Zeta,dt,t,teff,Gamma(i),Beta(i));
-
+[POS,VEL,ACC,T] = NewmarkBetaSolver(K,C,M,R,Zeta,dt,t,teff,Gamma(1),Beta(1));
 %% Plot 
 figure(2)
-fig_name1 = ['Position Plot Using Newmark Beta Method ',num2str(i),' for ',num2str(numofelem),' elements'];
-plot(T(1,:),POS(:,dof,:))
+fig_name1 = ['Position Plot Using Newmark Beta Method 1 for 50 elements'];
+plot(T(1,:),POS(:,121,:))
 grid on
 ylabel('\Theta_{z50}  (rad)')
 xlabel('t(s)')
 title(fig_name1)
 saveas(gcf,fig_name1,'jpg')
 
-figure(3)
-fig_name2 = ['Velocity Plot Using Newmark Beta Method ',num2str(i),' for ',num2str(numofelem),' elements'];
-plot(T(1,:),VEL(:,dof,:))
-grid on
-ylabel('d\Theta_{z50}/dt  (rad/s)')
-xlabel('t(s)')
-title(fig_name2)
-saveas(gcf,fig_name2,'jpg')
-
-figure(4)
-fig_name3 = ['Acceleration Plot Using Newmark Beta Method ',num2str(i),' for ',num2str(numofelem),' elements'];
-plot(T(1,:),ACC(:,dof,:))
-grid on
-ylabel('d^{2}\Theta_{z50}/dt^{2}  (rad/s^{2})')
-xlabel('t(s)')
-title(fig_name3)
-saveas(gcf,fig_name3,'jpg')
-end
+% i = 1;
+% for i=1:3
+% [POS,VEL,ACC,T] = NewmarkBetaSolver(K,C,M,R,Zeta,dt,t,teff,Gamma(i),Beta(i));
+% 
+% %% Plot 
+% figure(2)
+% fig_name1 = ['Position Plot Using Newmark Beta Method ',num2str(i),' for ',num2str(numofelem),' elements'];
+% plot(T(1,:),POS(:,dof,:))
+% grid on
+% ylabel('\Theta_{z50}  (rad)')
+% xlabel('t(s)')
+% title(fig_name1)
+% saveas(gcf,fig_name1,'jpg')
+% 
+% figure(3)
+% fig_name2 = ['Velocity Plot Using Newmark Beta Method ',num2str(i),' for ',num2str(numofelem),' elements'];
+% plot(T(1,:),VEL(:,dof,:))
+% grid on
+% ylabel('d\Theta_{z50}/dt  (rad/s)')
+% xlabel('t(s)')
+% title(fig_name2)
+% saveas(gcf,fig_name2,'jpg')
+% 
+% figure(4)
+% fig_name3 = ['Acceleration Plot Using Newmark Beta Method ',num2str(i),' for ',num2str(numofelem),' elements'];
+% plot(T(1,:),ACC(:,dof,:))
+% grid on
+% ylabel('d^{2}\Theta_{z50}/dt^{2}  (rad/s^{2})')
+% xlabel('t(s)')
+% title(fig_name3)
+% saveas(gcf,fig_name3,'jpg')
+% end
